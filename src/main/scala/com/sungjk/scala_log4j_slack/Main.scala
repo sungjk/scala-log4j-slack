e5411727b081f70bbdc4192800f9f049b98505c2
SungjungKim <ajax0615@gmail.com>
2017-09-21 11:13:16 +0900

    Fixed some typos

--- src/main/scala/com/sungjk/scala_log4j_slack/Main.scala
+++ src/main/scala/com/sungjk/scala_log4j_slack/Main.scala
@@ -29,7 +29,7 @@ object Main {
 		val responseFuture = ((confJson \ "host").asString, (confJson \ "endpoint").asString) match {
 			case (Some(host), Some(endpoint)) =>
 				val nettySlackService = new NettySlackService()
-				val logJson: JValue = "text" -> "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+				val logJson: JValue = "text" -> "Start log4j slack appender!!"
 				log.log(logJson)
 
 				nettySlackService.writeMessage(host, endpoint, logJson) flatMap {
